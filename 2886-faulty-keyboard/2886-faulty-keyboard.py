aa9ed0a5eb896e8d70cc792c9666073b659ddb72
Thomas11411 <pig11411@gmail.com>
2025-08-06 15:54:22 +0800

    Time: 50 ms (5.63%), Space: 16.4 MB (100%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+
+        res = []
+
+        for l in s:
+            if l == "i":
+                res = res[::-1]
+            else:
+                res.append(l)
+                
+        return ''.join(res)
