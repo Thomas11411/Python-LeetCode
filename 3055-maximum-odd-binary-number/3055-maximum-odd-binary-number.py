d5a34dfdb00847b06565b3324aa7df8fe51bb12c
Thomas11411 <pig11411@gmail.com>
2025-08-06 15:46:55 +0800

    Time: 42 ms (5.11%), Space: 16.1 MB (100%) - LeetHub

---
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        
+        from collections import Counter
+        d = Counter(s)
+        return "1" * (d["1"] - 1) + "0" * (d["0"]) + "1"
