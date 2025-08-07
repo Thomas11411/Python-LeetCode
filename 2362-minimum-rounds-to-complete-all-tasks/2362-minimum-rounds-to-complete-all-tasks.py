dead20434781935214ffc21b3576cd7d81d8ca19
Thomas11411 <pig11411@gmail.com>
2025-08-07 09:20:59 +0800

    Time: 1245 ms (5.9%), Space: 28.4 MB (100%) - LeetHub

--- 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.py
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.py
@@ -7,10 +7,11 @@ class Solution:
         for i in Counter(tasks).values():
             if i % 3 == 2:
                 res += (i - 2) // 3 + 1
-            elif i % 3 == 1:
-                if i == 1:
-                    return -1
+            elif i > 1 and i % 3 == 1:
                 res += (i - 4) // 3 + 2
-            else:
+            elif i % 3 == 0:
                 res += i // 3
+            else:
+                return -1
+        
         return res
