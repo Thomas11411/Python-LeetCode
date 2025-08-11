44c8ac1bbc8dde24459bbfa915561cc41297fc45
Thomas11411 <pig11411@gmail.com>
2025-08-11 14:11:28 +0800

    Time: 344 ms (9.12%), Space: 68.2 MB (7.29%) - LeetHub

---
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_employees(employees: pd.DataFrame) -> pd.DataFrame:
+    df = employees[employees.salary < 30000]
+    df = df[(~pd.isna(df.manager_id))]
+    result = pd.merge(employees, df, left_on = "employee_id", right_on = "manager_id", how = "right").reset_index()
+    result = pd.DataFrame(result.loc[pd.isna(result.employee_id_x), "employee_id_y"]).rename(columns = {"employee_id_y": "employee_id"}).sort_values("employee_id")
+    return result
