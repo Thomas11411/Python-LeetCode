21ced0e738c9ce76f9e5b1e6c277d7738209594d
Thomas11411 <pig11411@gmail.com>
2026-03-30 08:59:38 +0800

    Time: 1878 ms (5.03%), Space: 22.4 MB (89.51%) - LeetHub

--- 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.py
+++ 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.py
@@ -2,13 +2,16 @@ class Solution:
     def maximumXor(self, s: str, t: str) -> str:
         from collections import Counter
 
-        dt = Counter(list(t))
+        t = map(lambda x: int(x), t)
+        s = map(lambda x: int(x), s)
+
+        dt = Counter(t)
         res = ""
 
         for i,v in enumerate(s):
-            if dt[str(int(v) ^ 1)] > 0: 
+            if dt[v ^ 1] > 0: 
                 res += "1"
-                dt[str(int(v) ^ 1)] -= 1
+                dt[v ^ 1] -= 1
             else:
                 res += "0"
                 dt[v] -= 1
